9146cb82bc6058ae961b8b8e1234f1fa282b7b29
ClintPy <cmaruti93@gmail.com>
2019-12-16 11:23:33 +0300

    [Finished Setting Up Utils]

---
+++ utilis/bpdts.js
@@ -0,0 +1,26 @@
+// fetch list of persons from the bpdts API
+const fetch = require("node-fetch");
+
+class Fetch {
+  /**
+   * @descrip Returns a list of Persons from the bpdts API
+   * @returns {array} list of Persons
+   */
+  async getPersons() {
+    return fetch("https://bpdts-test-app.herokuapp.com/users")
+      .then(res => res.json())
+      .catch(err => res.send(err));
+  }
+
+  /**
+   * @descrip Returns a list of Persons Living in London
+   * @return {array} list of Persons living in London
+   */
+  async getLondonPersons() {
+    return fetch("https://bpdts-test-app.herokuapp.com/city/London/users")
+      .then(res => res.json())
+      .catch(err => res.send(err));
+  }
+}
+
+module.exports = Fetch;
