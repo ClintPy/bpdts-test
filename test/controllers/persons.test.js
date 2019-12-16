e41368e880b0d0485c3a8ff630208fbb4716e4aa
ClintPy <cmaruti93@gmail.com>
2019-12-16 18:42:33 +0300

    [Finish Test]

--- test/controllers/persons.test.js
+++ test/controllers/persons.test.js
@@ -2,6 +2,8 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../../app");
 
+const data = require('../mockData/data')
+
 const should = chai.should();
 
 chai.use(chaiHttp);
@@ -14,6 +16,7 @@ describe("/GET users", () => {
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a("array");
+        res.body.should.to.eql(data);
         done();
       });
   });
